bf92155e49ab60435a80e4713e0561cde79ac2da
Okies <ja00800@nate.com>
2018-08-14 17:26:50 +0900

    Commit Ch6

---
+++ Ch6/basicForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace study_CS_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string txt1 = textBox1.Text;
+            string txt2 = textBox2.Text;
+
+            if (txt1 == "AA" && txt2 == "BB")
+                MessageBox.Show("Success!!!", "Success");
+            else
+                MessageBox.Show("Denied", "Failure");
+        }
+    }
+}
