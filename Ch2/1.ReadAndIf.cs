d0337f3fbba6ad0178d87a4bb73c5d53a4a2913c
Okies <ja00800@nate.com>
2018-08-14 13:41:40 +0900

    Commit Ch2

---
+++ Ch2/1.ReadAndIf.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (;;)
+            {
+                Console.WriteLine("\n========== Menu ==========\n");
+                Console.WriteLine("1. One");
+                Console.WriteLine("2. Two");
+                Console.WriteLine("3. Three");
+                Console.WriteLine("4. Four");
+                Console.WriteLine("\nQ to Quit");
+
+                string ans = Console.ReadLine();
+
+                Console.WriteLine();
+
+                if (ans == "q" || ans == "Q")
+                    break;
+
+                if (ans == "1")
+                    Console.WriteLine("111");
+                else if (ans == "2")
+                    Console.WriteLine("222");
+                else if (ans == "3")
+                    Console.WriteLine("333");
+                else if (ans == "4")
+                    Console.WriteLine("444");
+                else
+                    Console.WriteLine("Invalid Input");
+            }
+        }
+    }
+}
---
+++ Ch2/2.ScoreToGrade.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (;;)
+            {
+                Console.Write("Score: ");
+                string ans = Console.ReadLine();
+
+                if (ans == "")
+                    break;
+
+                int score = int.Parse(ans);
+
+                if (score >= 90)
+                    Console.WriteLine("Grade A");
+                else if (score >= 80)
+                    Console.WriteLine("Grade B");
+                else if (score >= 70)
+                    Console.WriteLine("Grade C");
+                else if (score >= 60)
+                    Console.WriteLine("Grade D");
+                else
+                    Console.WriteLine("Grade F");
+            }
+        }
+    }
+}
