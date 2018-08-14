c8b0457bd6102584dcacdbb85f03397640511ecb
Okies <ja00800@nate.com>
2018-08-14 16:33:52 +0900

    Commit Ch4

---
+++ Ch4/1.String.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello";
+
+            char c = s[0];
+            Console.WriteLine(c);
+            Console.WriteLine(s[1]);
+            Console.WriteLine(s[2]);
+
+            string s2 = "World";
+
+            s = s + ", " + s2;
+            Console.WriteLine(s);
+
+            string sub = s.Substring(1, 4);
+            Console.WriteLine(sub);
+
+            s = "I am a boy";
+
+            string[] words = s.Split(' ');
+            foreach (var item in words)
+                Console.WriteLine(item);
+        }
+    }
+}
---
+++ Ch4/2.Namespace.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            study_CS.Program.Display("Hi");
+            Display("Hiii");
+
+            int result = study_CS.MyMath.Sum(1, 2);
+            study_CS.Program.Display(result);
+
+            result = MyMath.Sum(1, 2);
+            Display(result);
+        }
+
+        static void Display(object msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+
+    class MyMath
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
