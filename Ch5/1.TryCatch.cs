d3bd1a527944778d6f731414c93f398ddc979f8d
Okies <ja00800@nate.com>
2018-08-14 16:43:50 +0900

    Commit Ch5

---
+++ Ch5/1.TryCatch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 0;
+            
+            try
+            {
+                int c = a / b;
+                Console.WriteLine(c);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
