07e109519945fb0a8c4a919bd50d0cc55b8a5636
Okies <ja00800@nate.com>
2018-08-14 13:24:00 +0900

    Commit Ch1

---
+++ Ch1/2.ForLoop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 0; i <= 100; i++)
+                sum += i;
+
+            Console.WriteLine("Sum from 0 to 100 : {0}", sum);
+        }
+    }
+}
---
+++ Ch1/3.GuGuDan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                    Console.WriteLine("{0} x {1} = {2}", i, j, i * j);
+                Console.WriteLine("");
+            }
+        }
+    }
+}
